4036c04904d8b129efcad2c7cc2bcca589c78010
Ronny <Ronny@ADSP-13207>
2023-06-06 23:16:31 -0600

    Carga Inicial

---
+++ src/main/java/com/Practica2Web/Practica2WebApplication.java
@@ -0,0 +1,13 @@
+package com.Practica2Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practica2Web/Practica2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica2Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
